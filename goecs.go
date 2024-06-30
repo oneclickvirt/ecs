f7b00cf6211d1169f2031aeeff053763a61ff7da
spiritysdx <spiritlhl@yandex.com>
2024-06-30 13:19:32 +0800

    update

--- goecs.go
+++ goecs.go
@@ -101,11 +101,8 @@ func main() {
 	flag.StringVar(&diskTestMethod, "diskm", "fio", "Specify Disk test method (supported: fio, dd, winsat)")
 	flag.StringVar(&diskTestPath, "diskp", "", "Specify Disk test path, example: -diskp /root")
 	flag.BoolVar(&diskMultiCheck, "diskmc", false, "Enable multiple disk checks, example: -diskmc=false")
-	flag.Parse()
-	if language == "zh" {
-		flag.StringVar(&nt3Location, "nt3loc", "GZ", "指定三网回程路由检测的地址，支持 GZ, SH, BJ, CD 对应 广州，上海，北京，成都")
-		flag.StringVar(&nt3CheckType, "nt3t", "ipv4", "指定三网回程路由检测的类型，支持 both, ipv4, ipv6")
-	}
+	flag.StringVar(&nt3Location, "nt3loc", "GZ", "指定三网回程路由检测的地址，支持 GZ, SH, BJ, CD 对应 广州，上海，北京，成都")
+	flag.StringVar(&nt3CheckType, "nt3t", "ipv4", "指定三网回程路由检测的类型，支持 both, ipv4, ipv6")
 	flag.IntVar(&spNum, "spnum", 2, "Specify speedtest each operator servers num")
 	flag.Parse()
 	if showVersion {
@@ -118,12 +115,13 @@ func main() {
 	if language == "zh" {
 		printHead()
 		printCenteredTitle("基础信息", width)
-		wg.Add(2)
 		go func() {
+			wg.Add(1)
 			defer wg.Done()
 			basic.Basic(language)
 		}()
 		go func() {
+			wg.Add(1)
 			defer wg.Done()
 			securityInfo = securityCheck()
 		}()
@@ -134,16 +132,17 @@ func main() {
 		memorytest.MemoryTest(language, memoryTestMethod)
 		printCenteredTitle(fmt.Sprintf("硬盘测试-通过%s测试", diskTestMethod), width)
 		disktest.DiskTest(language, diskTestMethod, diskTestPath, diskMultiCheck)
-		wg.Add(1)
 		go func() {
+			wg.Add(1)
 			defer wg.Done()
 			emailInfo = email.EmailCheck()
 		}()
-		printCenteredTitle("御三家流媒体解锁", width)
 		go func() {
+			wg.Add(1)
 			defer wg.Done()
 			mediaInfo = mediatest(language)
 		}()
+		printCenteredTitle("御三家流媒体解锁", width)
 		commediatest.ComMediaTest(language)
 		printCenteredTitle("跨国流媒体解锁", width)
 		wg.Wait()
@@ -178,12 +177,13 @@ func main() {
 	} else if language == "en" {
 		printHead()
 		printCenteredTitle("Basic Information", width)
-		wg.Add(2)
 		go func() {
+			wg.Add(1)
 			defer wg.Done()
 			basic.Basic(language)
 		}()
 		go func() {
+			wg.Add(1)
 			defer wg.Done()
 			securityInfo = securityCheck()
 		}()
--- unlocktest/media.go
+++ unlocktest/media.go
@@ -1,24 +1,26 @@
 package unlocktest
 
 import (
-	"fmt"
 	"github.com/oneclickvirt/UnlockTests/utils"
 	"github.com/oneclickvirt/UnlockTests/uts"
 	"github.com/oneclickvirt/defaultset"
 )
 
 func MediaTest(language string) string {
+	var res string
 	readStatus := uts.ReadSelect(language, "0")
 	if !readStatus {
 		return ""
 	}
 	if uts.IPV4 {
-		fmt.Println(defaultset.Blue("IPV4:"))
-		return uts.RunTests(utils.Ipv4HttpClient, "ipv4", language, false)
+		res += defaultset.Blue("IPV4:") + "\n"
+		res += uts.RunTests(utils.Ipv4HttpClient, "ipv4", language, false)
+		return res
 	}
 	if uts.IPV6 {
-		fmt.Println(defaultset.Blue("IPV6:"))
-		return uts.RunTests(utils.Ipv6HttpClient, "ipv6", language, false)
+		res += defaultset.Blue("IPV6:") + "\n"
+		res += uts.RunTests(utils.Ipv6HttpClient, "ipv6", language, false)
+		return res
 	}
 	return ""
 }
