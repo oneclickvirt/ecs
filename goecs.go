955b887039ff5c6fb25209de893a826958e86bc4
spiritysdx <spiritlhl@yandex.com>
2024-07-04 18:53:30 +0800

    update

--- goecs.go
+++ goecs.go
@@ -37,7 +37,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.0.39"
+	ecsVersion                                                        = "v0.0.40"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
