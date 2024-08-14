73916d324a6a2fbd68f3775bb5dcc487b34bf813
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-08-14 20:34:24 +0800

    v0.0.72

--- goecs.go
+++ goecs.go
@@ -38,7 +38,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.0.71"
+	ecsVersion                                                        = "v0.0.72"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
