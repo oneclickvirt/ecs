ebce9e493d62a44400a006f9acc723a1da497026
spiritlhl <admin@spiritlhl.net>
2024-11-23 21:01:20 +0800

    v0.0.86 增加GooglePlay流媒体检测

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.0.85"
+	ecsVersion                                                        = "v0.0.86"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
