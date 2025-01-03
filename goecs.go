f5d9752e898bcbeee9440c32d1319a75de303e85
spiritlhl <admin@spiritlhl.net>
2025-01-03 16:44:55 +0800

    v0.1.3

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.2"
+	ecsVersion                                                        = "v0.1.3"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
