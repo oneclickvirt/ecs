eea78a7c3017e283faff442931243002a439f20d
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2025-01-31 12:29:06 +0800

    v0.1.10 - 更新流媒体检测

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.9"
+	ecsVersion                                                        = "v0.1.10"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
