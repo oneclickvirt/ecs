865ca62f06a3cb771b3abe2961679b51d2770e07
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2025-02-04 18:15:18 +0800

    加强部分检测

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.10"
+	ecsVersion                                                        = "v0.1.11"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
