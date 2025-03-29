23aae0b32b2c251f7c1627786539f493a2b25eb6
spiritsoul <spiritlhl@yandex.com>
2025-03-29 18:45:07 +0800

    fix: 迁移主main暂不使用的组件

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.20"
+	ecsVersion                                                        = "v0.1.21"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
--- goecs.sh
+++ goecs.sh
@@ -139,8 +139,8 @@ goecs_check() {
         sleep 1
     done
     if [ -z "$ECS_VERSION" ]; then
-        _yellow "Unable to get version info, using default version 0.1.19"
-        ECS_VERSION="0.1.19"
+        _yellow "Unable to get version info, using default version 0.1.21"
+        ECS_VERSION="0.1.21"
     fi
     # Check if original goecs command exists
     version_output=""
