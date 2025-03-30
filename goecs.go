42b3ab3cff1336a3cd110b4255cdb3821c8054f4
spiritsoul <spiritlhl@yandex.com>
2025-03-30 14:01:18 +0800

    fix: 同步修改

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.21"
+	ecsVersion                                                        = "v0.1.22"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
