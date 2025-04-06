dd6fbff943a451b1d7aac9f99cedac969225a12e
spiritsoul <spiritlhl@yandex.com>
2025-04-06 14:38:46 +0800

    fix: 替换嵌入的静态依赖不指定CPU指令集优化以兼容新旧CPU型号

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.27"
+	ecsVersion                                                        = "v0.1.28"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
