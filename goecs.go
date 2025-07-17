f47b1e30232453f22b9e5d2164c3d7712f17bc85
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-07-17 05:01:53 +0000

    fix: 不再使用goreleaser编译，直接使用原生runner进行编译

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.58"
+	ecsVersion                                                        = "v0.1.60"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
