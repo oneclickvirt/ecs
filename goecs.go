3c0590ca8d2d7028e7366a915175df7e08301dc6
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-07-17 02:43:28 +0000

    fix: 删除暂不适配的freebsd-arm

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.56"
+	ecsVersion                                                        = "v0.1.57"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
