5ec7924214df8290b9762f01865ebaa73e8896eb
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-08-08 14:26:31 +0000

    fix: 更新版本(mbw已回落可用但未能测出真实性能)

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.75"
+	ecsVersion                                                        = "v0.1.76"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
