b38dd713d95d1f5280fd44aa8731b1c30ce7f2c8
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-08-11 20:02:20 +0800

    fix: 修复退出选项被筛选逻辑拦截的问题

--- goecs.go
+++ goecs.go
@@ -40,7 +40,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.77"
+	ecsVersion                                                        = "v0.1.78"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
@@ -100,7 +100,7 @@ func getMenuChoice(language string) string {
 			if re.MatchString(input) {
 				inChoice := input
 				switch inChoice {
-				case "1", "2", "3", "4", "5", "6", "7", "8", "9", "10":
+				case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10":
 					return inChoice
 				default:
 					if language == "zh" {
