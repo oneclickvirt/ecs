4132b1daff7c8effebdbf4980ce479697d58599f
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-08-11 10:53:48 +0000

    feat: 详细三网路由测试支持并发测试，大幅缩短测试所需时间

--- goecs.go
+++ goecs.go
@@ -783,7 +783,39 @@ func runNetworkTests(wg3 *sync.WaitGroup, ptInfo *string, output, tempOutput str
 	output = utils.PrintAndCapture(func() {
 		if nt3Status && !onlyChinaTest {
 			utils.PrintCenteredTitle("三网回程路由检测", width)
-			nt.TraceRoute(language, nt3Location, nt3CheckType)
+			resultChan := make(chan nt.TraceResult, 100)
+			go nt.TraceRoute(language, nt3Location, nt3CheckType, resultChan)
+			for result := range resultChan {
+				if result.Index == -1 {
+					for index, res := range result.Output {
+						res = strings.TrimSpace(res)
+						if res != "" && index == 0 {
+							fmt.Println(res)
+						}
+					}
+					continue
+				}
+				if result.ISPName == "Error" {
+					for _, res := range result.Output {
+						res = strings.TrimSpace(res)
+						if res != "" {
+							fmt.Println(res)
+						}
+					}
+					return
+				}
+				for _, res := range result.Output {
+					res = strings.TrimSpace(res)
+					if res == "" {
+						continue
+					}
+					if strings.Contains(res, "ICMP") {
+						fmt.Print(res)
+					} else {
+						fmt.Println(res)
+					}
+				}
+			}
 		}
 	}, tempOutput, output)
 	return utils.PrintAndCapture(func() {
