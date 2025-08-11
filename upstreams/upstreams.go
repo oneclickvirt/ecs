a99f58518a9c3506f4fae4d30c77612c22795075
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-08-11 11:13:07 +0000

    fix: 升级backtrace依赖，支持多重路由测试取平均结果，避免单次路由波动分析失败

--- upstreams/upstreams.go
+++ upstreams/upstreams.go
@@ -2,19 +2,65 @@ package upstreams
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/oneclickvirt/UnlockTests/uts"
 	bgptools "github.com/oneclickvirt/backtrace/bgptools"
 	backtrace "github.com/oneclickvirt/backtrace/bk"
+	. "github.com/oneclickvirt/defaultset"
 )
+
+type IpInfo struct {
+	Ip      string `json:"ip"`
+	City    string `json:"city"`
+	Region  string `json:"region"`
+	Country string `json:"country"`
+	Org     string `json:"org"`
+}
+
+type ConcurrentResults struct {
+	bgpResult       string
+	backtraceResult string
+	bgpError        error
+	// backtraceError  error
+}
 
 var IPV4, IPV6 string
 
 func UpstreamsCheck() {
+	results := ConcurrentResults{}
+	var wg sync.WaitGroup
 	if IPV4 != "" {
-		if result, err := bgptools.GetPoPInfo(IPV4); err == nil {
-			fmt.Print(result.Result)
-		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 2; i++ {
+				result, err := bgptools.GetPoPInfo(IPV4)
+				results.bgpError = err
+				if err == nil && result.Result != "" {
+					results.bgpResult = result.Result
+					return
+				}
+				if i == 0 {
+					time.Sleep(3 * time.Second)
+				}
+			}
+		}()
 	}
-	backtrace.BackTrace(uts.IPV6)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		result := backtrace.BackTrace(uts.IPV6)
+		results.backtraceResult = result
+	}()
+	wg.Wait()
+	if results.bgpResult != "" {
+		fmt.Print(results.bgpResult)
+	}
+	if results.backtraceResult != "" {
+		fmt.Printf("%s\n", results.backtraceResult)
+	}
+	fmt.Println(Yellow("准确线路自行查看详细路由，本测试结果仅作参考"))
+	fmt.Println(Yellow("同一目标地址多个线路时，检测可能已越过汇聚层，除第一个线路外，后续信息可能无效"))
 }
