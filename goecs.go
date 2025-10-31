b403d71115996b2c2f25274b26ee29001ff1791d
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-10-31 07:15:38 +0000

    fix：更新版本

--- goecs.go
+++ goecs.go
@@ -39,7 +39,7 @@ import (
 )
 
 var (
-	ecsVersion                                                        = "v0.1.91"
+	ecsVersion                                                        = "v0.1.92"
 	menuMode                                                          bool
 	onlyChinaTest                                                     bool
 	input, choice                                                     string
